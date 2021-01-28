9ca332656c1c6fafd6a930be5ab70f68c5be7cfe
Alexander <ubuntu@yandex.ru>
2021-01-28 15:16:33 +0300

    add x,y print math action

---
+++ src/1 course/1 var.py
@@ -0,0 +1,11 @@
+x=5
+y=3
+print("x =", x , "; y=",y)
+print("x+y =", x+y)
+print("x-y =",x-y)
+print("x*y =",x*y)
+print("x/y =",x/y)
+print("x//y =",x//y)
+print("x%y =",x%y)
+print("x**y =",x**y)
+
