87904122995bd072d8edcc7e3f415488aba93aed
Alexander <ubuntu@yandex.ru>
2021-01-28 15:16:33 +0300

    add credit

--- src/1 course/1 var.py
+++ src/1 course/1 var.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 x=5
 y=3
 print("x =", x , "; y=",y)
@@ -9,3 +10,25 @@ print("x//y =",x//y)
 print("x%y =",x%y)
 print("x**y =",x**y)
 
+=======
+# x=5
+# y=3
+# print("x =", x , "; y=",y)
+# print("x+y =", x+y)
+# print("x-y =",x-y)
+# print("x*y =",x*y)
+# print("x/y =",x/y)
+# print("x//y =",x//y)
+# print("x%y =",x%y)
+# print("x**y =",x**y)
+
+credit = 1000000
+cr_rate = 12.9
+cr_years = 2
+
+final_sum = credit + credit / 100 * cr_rate * cr_years
+mount_pay = final_sum /  (cr_years * 12)
+
+print(final_sum)
+print(round(mount_pay,2))
+>>>>>>> add credit
